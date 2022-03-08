13e36c4538e3f16ee0621be35e866740b7f4ab8b
joao rosa <joaopaulosrosa@gmail.com>
2022-03-08 15:09:35 -0300

    buttons updating items quantity

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.store, name='store'),
     path('product/<str:pk>', views.product, name='product'),
     path('cart', views.cart, name='cart'),
-    path('checkout', views.checkout, name='checkout')
+    path('checkout', views.checkout, name='checkout'),
+
+    path('update_item/', views.updateItem, name='update_item')
 
 ]
